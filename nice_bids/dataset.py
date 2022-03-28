c76a9edc229b2f0c304e518446979f5d452e1143
Laouen <laouen.belloli@gmail.com>
2022-03-28 15:05:03 +0200

    Allowing lists for subseting

--- nice_bids/dataset.py
+++ nice_bids/dataset.py
@@ -1,9 +1,10 @@
-from typing import List
+from typing import List, Union
 
 import pandas as pd
 import json
 import os
 from glob import glob
+import re
 
 from functools import partial
 from multiprocessing import cpu_count
@@ -29,8 +30,10 @@ def load_derivative(filepath, participants):
 class NICEBIDS:
 
     def __init__(self, root:str, 
-                 sub:str=None, ses:str=None,
-                 task:str=None, acq:int=None,
+                 sub:Union[str,List[str]]=None,
+                 ses:Union[str,List[str]]=None,
+                 task:Union[str,List[str]]=None,
+                 acq:Union[int,List[int]]=None,
                  derivatives:List[str]=None, rjust:int=2) -> None:
 
         self.subset = {
@@ -39,7 +42,29 @@ class NICEBIDS:
             'task': task,
             'acq': acq
         }
+
+        # Filter and parse list string and single values to list
         self.subset = {k:v for k,v in self.subset.items() if v is not None}
+        self.subset = {
+            k: v.replace(', ',',').split(',') if isinstance(v,str) else v 
+            for k,v in self.subset.items()
+        }
+
+        # build subset regex pattern for filtering
+        subs = '(' + '|'.join(self.subset.get('sub', ['.+'])) + ')'
+        sess = '(' + '|'.join(self.subset.get('ses', ['.+'])) + ')'
+        tasks = '(' + '|'.join(self.subset.get('task', ['.+'])) + ')'
+        acqs = self.subset.get('acq', ['.+'])
+        if acqs[0] != '.+':
+            acqs = [str(acq).rjust(self.rjust, '0') for acq in acqs]
+        acqs = '(' + '|'.join(acqs) + ')'
+        self.subset_regex_pattern = os.path.join(
+            f'sub-{subs}',
+            f'ses-{sess}',
+            'eeg',
+            f'sub-{subs}_ses-{sess}_task-{tasks}_acq-{acqs}_eeg.*$' # <- match end of string
+        )
+        
         self.rjust = rjust
         self.root = root
         self.participants_descriptions = None
@@ -61,8 +86,8 @@ class NICEBIDS:
 
         # Remove participants not selected
         if 'sub' in self.subset:
-            sub = self.subset['sub']
-            fileter_mask = self.participants.index == f'sub-{sub}'
+            subs = [f'sub-{sub}' for sub in self.subset['sub']]
+            fileter_mask = self.participants.index.isin(subs)
             self.participants = self.participants[fileter_mask]
 
         participants_json = os.path.join(self.root, 'participants.json')
@@ -73,25 +98,22 @@ class NICEBIDS:
     def _read_files(self, n_jobs=None):
         print('Loading data')
         
-        sub = self.subset.get('sub', '*')
-        ses = self.subset.get('ses', '*')
-        task = self.subset.get('task', '*')
-        acq = self.subset.get('acq', '*')
-
-        if acq != '*':
-            acq = str(acq).rjust(self.rjust, '0')
-        
         self.files = glob(os.path.join(
             self.root,
-            f'sub-{sub}',
-            f'ses-{ses}',
+            f'sub-*',
+            f'ses-*',
             'eeg',
-            f'sub-{sub}_ses-{ses}_task-{task}_acq-{acq}*_eeg.*'
+            f'sub-*_ses-*_task-*_eeg.*'
         ))
 
         # Filter incorrect recording filenames
-        self.files = list(filter(BIDSPath.correct_filepath, self.files))
+        self.files = filter(BIDSPath.correct_filepath, self.files)
         
+        # Filter by selected subset
+        pattern = os.path.join(self.root, self.subset_regex_pattern)
+        print(pattern)
+        self.files = [file for file in self.files if re.match(pattern, file)]
+
         load_file_func = partial(
             load_path,
             participants=self.participants
@@ -106,7 +128,7 @@ class NICEBIDS:
             leave=False
         )
 
-    def _read_derivatives(self, derivatives:list[str]=None, n_jobs:int=None):
+    def _read_derivatives(self, derivatives:List[str]=None, n_jobs:int=None):
         derivative_root = os.path.join(self.root, 'derivatives')
         if not os.path.exists(derivative_root):
             print('There is no derivatives folder. Reading derivative skiped')
@@ -114,34 +136,28 @@ class NICEBIDS:
 
         print('Reading derivatives')
 
-        sub = self.subset.get('sub', '*')
-        ses = self.subset.get('ses', '*')
-        task = self.subset.get('task', '*')
-        acq = self.subset.get('acq', '*')
-
-        all_files = glob(os.path.join(
+        files = glob(os.path.join(
             derivative_root,
             '*',
-            f'sub-{sub}',
-            f'ses-{ses}',
+            f'sub-*',
+            f'ses-*',
             'eeg',
-            f'sub-{sub}_ses-{ses}_task-{task}_acq-{acq}*_*.*'
+            f'sub-*_ses-*_task-*_*.*'
         ))
 
-        if derivatives is not None:
-            files = []
-            for folder in derivatives:
-                files += [
-                    file for file in files
-                    if file.startswith(os.path.join(derivative_root, folder))
-                ]
-        else:
-            files = all_files
+        # Filter derivatives by subset and folders
+        derivatives = '(' + '|'.join(derivatives) + ')' if derivatives else '.+'
+        pattern = os.path.join(
+            derivative_root,
+            derivatives,
+            self.subset_regex_pattern
+        )
+        derivative_files = [file for file in files if re.match(pattern, file)]
 
-        derivative_files = list(filter(
-            lambda f: BIDSPath.correct_filepath(f, 'derivative'),
-            files
-        ))
+        derivative_files = [
+            file for file in derivative_files
+            if BIDSPath.correct_filepath(file, 'derivative')
+        ]
         
         load_derivative_func = partial(
             load_derivative,
@@ -201,7 +217,7 @@ class NICEBIDS:
             acq=acq, run=run, suffix=suffix
         )
         
-        return list(filter(query, self.files))
+        return [file for file in self.files if query(file)]
     
     def to_df(self, sub:str=None, task:str=None, ses:str=None,
                     acq:int=None, run:int=None):
@@ -233,7 +249,7 @@ class NICEBIDS:
             acq=acq, run=run, suffix=suffix, derivative=derivative
         )
 
-        return list(filter(query, self.derivative_files))
+        return [file for file in self.derivative_files if query(file)]
 
     def __repr__(self) -> str:
         return f'Subjects: {len(self.participants)}, files: {len(self.files)}'
